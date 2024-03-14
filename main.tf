a27d1294a01e0374e29a2edd86d01d16161fad7e
Nargiza <r.nakosya@gmail.com>
2024-03-14 15:27:26 -0500

    added ecr code

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws"{
+  region = "us-west-2"
+}
+resource "aws_ecr_repository" "foo" {
+  name                 = "bar"
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
